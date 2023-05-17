3c192fb431d23502e0d9eafc1638590be33dcb7c
Alina <lina.oskolkova.97@mail.ru>
2023-05-17 10:46:17 +0600

    test

--- test.py
+++ test.py
@@ -17,15 +17,15 @@ class TestApp(unittest.TestCase):
     def test_form_route_with_post_request_and_option1(self):
         response = self.client.post('/', data={'summ': '2000000', 'first_installment': '500000', 'month': '15',
                                                'percent': '12', 'gridRadios': 'option1'})
-        self.assertIn(bytes("Ежемесячный платёж:", encoding='utf-8'), response.data)
-        self.assertIn(bytes('Cумма кредита:', encoding='utf-8'), response.data)
+        self.assertIn(b'18003', response.data)
+        self.assertIn(b'1500000', response.data)
 
     # проверка данных с option2
     def test_form_route_with_post_request_and_option2(self):
         response = self.client.post('/', data={'summ': '2000000', 'first_installment': '500000', 'month': '15',
                                                'percent': '12', 'gridRadios': 'option2'})
-        self.assertIn(bytes("Ежемесячный платёж:", encoding='utf-8'), response.data)
-        self.assertIn(bytes('Cумма кредита:', encoding='utf-8'), response.data)
+        self.assertIn(b"23333", response.data)
+        self.assertIn(b'1500000', response.data)
 
     if __name__ == '__main__':
         unittest.main()
